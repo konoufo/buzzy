ce7ad6fdc7aae11a16d165d65011aee0c5ccffd0
konoufo <konoufo1@gmail.com>
2017-03-19 23:47:42 -0400

    working buzzer app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var version = require('./package.json').version,
     express = require('express'),
     bodyParser = require('body-parser'),
+    player = require('play-sound')(opts={}),
     app = express(),
+    audio = false,
     locked = false,
     winner = null,
     noWords = /\W/;
@@ -11,9 +13,18 @@ process.stdin.setEncoding('utf8');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-function clear () { process.stdout.write('\033c'); }
-function beep () { process.stdout.write('\x07'); };
+app.use(express.static('./public'));
+
+function clear () { audio && audio.kill && audio.kill(); process.stdout.write('\033c'); }
+function beep () { 
+	audio = player.play('./music/buzzer.mp3', {maxBuffer: 500 * 1024}, function(err){
+		if (err && !err.killed) throw err;
+    err && console.log(err);
+	}); 
+	process.stdout.write('\x07'); };
 
 // keyboard entry
 process.stdin.on('data', function(key) {
@@ -22,7 +33,9 @@ process.stdin.on('data', function(key) {
     case '\u001B': // esc
       clear();
       console.log('Ready…');
+      winner = null;
       locked = false;
+      audio = false;
       break;
     default:
       if (locked || noWords.test(key)) break;
@@ -37,18 +50,24 @@ process.stdin.on('data', function(key) {
 // HTTP POST entry
 app.post('/', function(req, res){
   if (!req.body.name) return res.status(400).send('`name` body field required');
-  if (locked) return res.send('Sorry, ' + winner + ' was faster.');
+  if (locked) return res.send(winner);
   locked = true;
   winner = req.body.name;
   beep();
   clear();
   console.log('THE WINNER IS: ', req.body.name);
-  res.send('YOU ARE THE WINNER!');
+  res.send(winner);
+});
+
+app.get('/', function(req, res){
+    res.render('home', {
+      teams: ['Congo', 'Côte d\'Ivoire', 'Rojava', 'Sénégal', 'Togo'],
+    });
 });
 
 // initialize
 clear();
-console.log('Fancy lockout buzzer version', version);
+console.log('Buzzer lockout ACAFUL version', version);
 
 app.listen(1337, function () {
   console.log('Listening on 1337');
